234b0805d82ec7397dff14f74e70cf702ce0290b
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-10 16:14:40 -0400

    variables de entorno

---
+++ database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection( {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+    if(err){
+        console.log(err.message);
+        return;
+    }
+    console.log("Base de Datos Conectada")
+});
