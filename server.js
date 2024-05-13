fb0fb8fee72d1816da7773fe56c1d6fa688513ec
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-13 11:26:27 -0400

    arreglando rutas

--- server.js
+++ server.js
@@ -62,8 +62,10 @@ app.use((req, res, next) => {
   });
 
 
-// Ruta raíz que redirige al usuario a login.html
-app.get('/', (req, res) => {
+
+
+
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', '/login/login.html'));
 });
 
