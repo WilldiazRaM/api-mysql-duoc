756d615467b3df03e7696f5a91ca82dd9362f6a1
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-12 21:23:33 -0400

    database raiz server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const pool = require('./database');
+const pool = require('database');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const helmet = require('helmet'); //Strict-transport-security'
