a1809fd9562f55e4a5ac30697052e346ecd1da08
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-13 11:30:56 -0400

    Se corige politicas

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "script-src 'self';");
+    res.setHeader('Content-Security-Policy', "script-src 'self' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline'");
     next();
 });
 
