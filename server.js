5ca4fb5a51a9007c1eae70b37690f17fab2e9ce4
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-13 16:21:25 -0400

    config bodyParser

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(helmet());
 app.use(morgan('combined'));  // Guarda log de las solicitudes
 
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
