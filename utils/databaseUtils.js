4ec74f11d7c3883466af11c3851029510896dd65
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-13 14:15:54 -0400

    se impota hashPassword function

--- utils/databaseUtils.js
+++ utils/databaseUtils.js
@@ -1,4 +1,5 @@
 const pool = require('../database');
+const { hashPassword, comparePasswords } = require('../passwordUtils');
 
 async function findByEmail(email) {
     return new Promise((resolve, reject) => {
@@ -38,23 +39,32 @@ function createUser(user) {
     return new Promise((resolve, reject) => {
         const { nombre, email, password } = user;
         console.log("Datos de usuario:", nombre, email, password);
-        const query = 'INSERT INTO Usuarios (nombre, email, password, role, created_at) VALUES (?, ?, ?, ?, NOW())';
-        console.log("Query SQL:", query);
         
-        // Asegúrate de que role esté definido si no se proporciona en el objeto user
-        const role = user.role || 'cliente';
-        
-        pool.query(query, [nombre, email, password, role], (error, result) => {
-            if (error) {
-                console.error("Error en createUser:", error);
-                return reject(error);
-            }
-            const newUserId = result.insertId;
-            console.log("ID de usuario creado:", newUserId);
+        // Generar hash de la contraseña
+        hashPassword(password)
+            .then(hashedPassword => {
+                const query = 'INSERT INTO Usuarios (nombre, email, password, role, created_at) VALUES (?, ?, ?, ?, NOW())';
+                console.log("Query SQL:", query);
+                
+                
+                const role = user.role || 'cliente';
+                
+                pool.query(query, [nombre, email, hashedPassword, role], (error, result) => {
+                    if (error) {
+                        console.error("Error en createUser:", error);
+                        return reject(error);
+                    }
+                    const newUserId = result.insertId;
+                    console.log("ID de usuario creado:", newUserId);
 
-            // Retorna el nuevo usuario con el ID asignado
-            resolve({ id: newUserId, nombre, email, password, role, created_at: new Date() });
-        });
+                    // Retorna el nuevo usuario con el ID asignado y sin la contraseña
+                    resolve({ id: newUserId, nombre, email, role, created_at: new Date() });
+                });
+            })
+            .catch(error => {
+                console.error("Error al cifrar la contraseña:", error);
+                reject(error);
+            });
     });
 };
 
