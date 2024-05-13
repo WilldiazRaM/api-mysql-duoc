6da26c4f86d92d1938816802fe54c6ce9f25a140
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-12 20:12:48 -0400

    se agrega funcion para crearUsuarios

--- utils/databaseUtils.js
+++ utils/databaseUtils.js
@@ -9,8 +9,32 @@ async function findByemail(email) {
         return rows[0];
     }catch(err){
         throw err;
-    };
-} 
+    }
+}; 
 
 
-module.exports = { findByemail };
+async function createUser(user) {
+    return new Promise((resolve, reject) => {
+        
+        const newUserId = records.length + 1;
+
+        // Crea el nuevo usuario con el ID asignado
+        const newUser = {
+            id: newUserId,
+            ...user,
+        };
+
+        // Añade el nuevo usuario a la lista de registros
+        records.push(newUser);
+
+        // Muestra los registros actualizados en la consola (opcional)
+        console.log(records);
+
+        
+        resolve(newUser);
+    });
+}
+
+
+
+module.exports = { findByemail, createUser };
