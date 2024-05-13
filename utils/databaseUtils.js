1cae70819f61ead497b5b668c8253ad9f043248a
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-13 17:24:58 -0400

    modificacion en un funciones utils

--- utils/databaseUtils.js
+++ utils/databaseUtils.js
@@ -35,58 +35,37 @@ async function findUserById(id) {
 };
 
 
-function createUser(user) {
-    return new Promise((resolve, reject) => {
-        const { nombre, email, password } = user;
-        console.log("Datos de usuario:", nombre, email, password);
-        
-        // Generar hash de la contraseña
-        hashPassword(password)
-            .then(hashedPassword => {
-                const query = 'INSERT INTO Usuarios (nombre, email, password, role, created_at) VALUES (?, ?, ?, ?, NOW())';
-                console.log("Query SQL:", query);
-                
-                
-                const role = user.role || 'cliente';
-                
-                pool.query(query, [nombre, email, hashedPassword, role], (error, result) => {
-                    if (error) {
-                        console.error("Error en createUser:", error);
-                        return reject(error);
-                    }
-                    const newUserId = result.insertId;
-                    console.log("ID de usuario creado:", newUserId);
-
-                    // Retorna el nuevo usuario con el ID asignado y sin la contraseña
-                    resolve({ id: newUserId, nombre, email, role, created_at: new Date() });
-                });
-            })
-            .catch(error => {
-                console.error("Error al cifrar la contraseña:", error);
-                reject(error);
+async function createUser(email, password) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const hashedPassword = await hashPassword(password);
+            const role = 'cliente'; // Asignar un rol predeterminado
+            const query = 'INSERT INTO Usuarios (email, password, role, created_at) VALUES (?, ?, ?, NOW())';
+            pool.query(query, [email, hashedPassword, role], (error, result) => {
+                if (error) {
+                    console.error("Error en createUser:", error);
+                    return reject(error);
+                }
+                const newUserId = result.insertId;
+                resolve({ id: newUserId, email, role, created_at: new Date() });
             });
+        } catch (error) {
+            console.error("Error al crear usuario:", error);
+            reject(error);
+        }
     });
-};
-
-
+}
 
 async function authenticateUser(email, password) {
     try {
-        // Buscar el usuario por su correo electrónico
         const user = await findByEmail(email);
-        
         if (!user) {
-            return null; // Si no se encuentra el usuario, devolver null
+            return null;
         }
-        
-        // Comparar la contraseña ingresada con la contraseña almacenada en la base de datos
         const passwordMatch = await comparePasswords(password, user.password);
-        
         if (!passwordMatch) {
-            return null; // Si las contraseñas no coinciden, devolver null
+            return null;
         }
-        
-        // Si el usuario y la contraseña son válidos, devolver el usuario
         return user;
     } catch (error) {
         console.error("Error durante la autenticación:", error);
