687fd0bc2e5e96c4eaae04caabdce4b92b9d75f1
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-14 14:25:49 -0400

    Axios

--- routes/carritoRouters.js
+++ routes/carritoRouters.js
@@ -68,3 +68,5 @@ router.delete('/carrito/:id_detalle', async (req, res) => {
         res.status(500).json({ error: "Ocurrió un error al eliminar el producto del carrito" });
     }
 });
+
+module.exports = router;
---
+++ routes/pagosRoutes.js
@@ -0,0 +1,3 @@
+const pool = require('../database');
+const express = require('express');
+const router = express.Router();
