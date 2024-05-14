e3468ce82f6a3c9730e225d7a29700ced8fcd96f
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-14 01:59:55 -0400

    importa prductosRouters.js4

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const passport = require('passport');
 const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const store = new session.MemoryStore();
-const productosRouter = require('./utils/productosRouter');
-
+const productosRouter = require('./utils/productoUtils');
 
 
 
