a0661a12bc80ec2bd5137e8b596d3f71cf0ad144
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-14 11:42:04 -0400

    Se mapea categorias segun db aws mysqp

--- utils/productoUtils.js
+++ utils/productoUtils.js
@@ -1,19 +1,45 @@
 const pool = require('../database');
 
+
+const categoriasId = {
+    'Herramientas Manuales': 1,
+    'Herramientas Eléctricas': 2,
+    'Materiales de Construcción': 3,
+    'Acabados': 4,
+    'Equipos de Seguridad': 5,
+    'Tornillos y Anclajes': 6,
+    'Fijaciones y Adhesivos': 7,
+    'Equipos de Medición': 8
+};
+
 async function createProducto(nombre, precio, descripcion, stock, categoria_nombre) {
     return new Promise((resolve, reject) => {
-        const query = 'INSERT INTO Productos (nombre, precio, descripcion, stock, categoria_nombre) VALUES (?, ?, ?, ?, ?)';
-        pool.query(query, [nombre, precio, descripcion, stock, categoria_nombre], (error, result) => {
+        // Obtener el id de la categoría correspondiente al nombre de la categoría
+        const categoria_id = categoriasId[categoria_nombre];
+        if (!categoria_id) {
+            // Si la categoría no existe en el diccionario, rechazar la promesa con un error
+            const error = new Error('La categoría especificada no existe');
+            console.error("Error en createProducto:", error);
+            return reject(error);
+        }
+
+        // Realizar la inserción del producto con el id de la categoría obtenido
+        const query = 'INSERT INTO Productos (nombre, precio, descripcion, stock, categoria_id) VALUES (?, ?, ?, ?, ?)';
+        pool.query(query, [nombre, precio, descripcion, stock, categoria_id], (error, result) => {
             if (error) {
                 console.error("Error en createProducto:", error);
                 return reject(error); // Rechazar la promesa con el error
             }
             const newProductId = result.insertId;
             // Resolver con los datos del producto registrado
-            resolve({ id: newProductId, nombre, precio, descripcion, stock, categoria_nombre });
+            resolve({ id: newProductId, nombre, precio, descripcion, stock, categoria_id });
         });
     });
 }
+
+
+
+
 
 module.exports = {
     createProducto
