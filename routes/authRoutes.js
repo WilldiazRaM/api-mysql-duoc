cd4c4b0f52e969262aec84916328ac6c5b1601fd
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-21 03:00:04 -0400

    okokok

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,25 +2,36 @@ const express = require('express');
 const router = express.Router();
 const { login, register, logout } = require('../controllers/authController');
 const passport = require('./passportConfig');
-const { isAuthenticated } = require('../utils/passwordUtils');
 
 // Ruta para iniciar sesión local
 router.post('/login', login);
 router.post('/registrar', register); 
 router.get('/logout', logout);
 
+
+//GOOGLE
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/auth/login' }), (req, res) => {
-    res.redirect('/profile');
+router.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: '/auth/login'
+}), (req, res) => {
+    // Generar JWT después de la autenticación exitosa
+    const token = jwt.sign({ id: req.user.id, provider: 'google' }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.redirect(`/profile?token=${token}`);
 });
 
-router.get('/github', passport.authenticate('github'));
-router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/auth/login' }), (req, res) => {
-    res.redirect('/profile');
+//GITHUB
+router.get('/github', passport.authenticate('github')); 
+router.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/auth/login'
+}), (req, res) => {
+    // Generar JWT después de la autenticación exitosa
+    const token = jwt.sign({ id: req.user.id, provider: 'github' }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.redirect(`/profile?token=${token}`);
 });
 
 router.get('/login', (req, res) => {
     res.sendFile('login.html', { root: './public/login' });
 });
+
 
 module.exports = router;
