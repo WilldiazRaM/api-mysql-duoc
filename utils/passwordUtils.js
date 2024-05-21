a2897b17d7c413169f117332d29bbfa19ab7fdd7
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-21 02:01:15 -0400

    added jwt

--- utils/passwordUtils.js
+++ utils/passwordUtils.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET || 'secretoSuperSeguro';
+
 
 async function hashPassword(password) {
     const saltRounds = 10;
@@ -29,16 +31,32 @@ function requireAuth(JWT_SECRET) {
 
 // Middleware para verificar si el usuario está autenticado
 function isAuthenticated(req, res, next) {
+    // Verificar si está autenticado por Passport
     if (req.isAuthenticated()) {
         return next();
     }
 
+    // Verificar si el usuario está autenticado a través de Google o GitHub
     if (req.user && (req.user.provider === 'google' || req.user.provider === 'github')) {
         return next();
     }
 
-    res.redirect('/auth/login');
+    // Verificar autenticación JWT
+    const token = req.headers['x-access-token'] || req.query.token || req.cookies.token;
+    if (token) {
+        jwt.verify(token, JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.redirect('/auth/login');
+            } else {
+                req.user = decoded;
+                return next();
+            }
+        });
+    } else {
+        // Si no hay ningún token, redirigir a la página de inicio de sesión
+        console.log("no hay ningún token");
+        res.redirect('/auth/login');
+    }
 }
-
 
 module.exports = { hashPassword, comparePasswords, requireAuth, isAuthenticated };
