303a1e3c3c2c4d7e063569ecc7451459547ded89
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-21 14:49:52 -0400

    se arregla orden de session2

--- server.js
+++ server.js
@@ -1,51 +1,43 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 const session = require('express-session');
 const store = new session.MemoryStore();
-const helmet = require('helmet');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 const authRoutes = require('./routes/authRoutes');
-
 const productosRoutes = require('./routes/productosRoutes');
 const pagosRouter = require('./routes/pagosRoutes');
 const historialesRoutes = require('./routes/historialRoutes');
 const carritoRouter = require('./routes/carritoRouters');
 const { isAuthenticated } = require('./utils/passwordUtils');
 
-
-
-
 // Servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use(helmet());
-app.use(morgan('combined'));  // Guarda log de las solicitudes
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 const PORT = process.env.PORT || 4001;
 
+// Configuración de sesión
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: sessionStore,
+    store: store,
     cookie: { maxAge: 86400000, secure: process.env.NODE_ENV === 'production' }
 }));
 
-
-
+// Inicialización de Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
+// Middleware de seguridad
 app.use((req, res, next) => {
     res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubdomains');
     res.setHeader('Content-Security-Policy', "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; script-src 'self' https://cdn.jsdelivr.net");
@@ -55,15 +47,7 @@ app.use((req, res, next) => {
     next();
 });
 
-
-
-
-
-
-
-
-
-//Rutas LOGIN
+// Rutas
 app.use('/auth', authRoutes);
 app.use('/productos', productosRoutes);
 app.use('/pagos', pagosRouter);
@@ -75,19 +59,13 @@ app.get('/profile', isAuthenticated, (req, res) => {
     res.sendFile('profile.html', { root: './public/login' });
 });
 
-
-
 // Middleware de manejo de errores global
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
 });
 
-
-
-
+// Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
-
 });
-
