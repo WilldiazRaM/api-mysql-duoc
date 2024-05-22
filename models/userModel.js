358c1341830a3dc49799561daa6ee9b7e07706ac
WilldiazRaM <wi.diaz@duocuc.cl>
2024-05-22 00:06:09 -0400

    ASDASDA

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const pool = require('../database');
+const { comparePasswords } = require('../utils/passwordUtils');
 
 async function findUserByEmail(email) {
     const query = 'SELECT * FROM Usuarios WHERE email = ?';
@@ -6,17 +7,29 @@ async function findUserByEmail(email) {
     if (rows.length > 0) {
         return rows[0];
     } else {
-        return null; // O cualquier otro valor que indique que el usuario no fue encontrado
+        return null;
     }
 }
-
 
 async function createUser(email, hashedPassword) {
     const query = 'INSERT INTO Usuarios (email, password) VALUES (?, ?)';
     await pool.query(query, [email, hashedPassword]);
 }
 
+async function authenticateUser(email, password) {
+    const user = await findUserByEmail(email);
+    if (!user) {
+        return null;
+    }
+    const isPasswordValid = await comparePasswords(password, user.password);
+    if (!isPasswordValid) {
+        return null;
+    }
+    return user;
+}
+
 module.exports = {
     findUserByEmail,
-    createUser
+    createUser,
+    authenticateUser
 };
