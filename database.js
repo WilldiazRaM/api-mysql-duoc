a467979463fab44670a34612b63233802c66efc3
WilldiazRaM <wi.diaz@duocuc.cl>
2024-06-20 14:41:19 -0400

    changed pg database

--- database.js
+++ database.js
@@ -1,11 +1,23 @@
-const mysql = require('mysql2/promise');
+const { Pool } = require('pg');
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  ssl: {
+    rejectUnauthorized: false 
+  }
+});
+
+// Manejar la conexión a la base de datos
+pool.on('connect', () => {
+  console.log('Conexión exitosa a la base de datos PostgreSQL');
+});
+
+pool.on('error', (error) => {
+  console.error('Error en la conexión a la base de datos PostgreSQL:', error);
 });
 
 module.exports = pool;
