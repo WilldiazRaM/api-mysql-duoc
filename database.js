f4035bfb354e7272f98e5e0a59ff1418aa0ed271
WilldiazRaM <wi.diaz@duocuc.cl>
2024-06-24 02:36:39 -0400

    se pasa a la bd psotgreSql forma local notebook

--- database.js
+++ database.js
@@ -1,14 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  ssl: {
-    rejectUnauthorized: false 
-  }
+  host: 'localhost',
+  port: 5432,
+  user: 'will',
+  password: 'Will88',
+  database: 'pruebasunitariasapiduoc',
+  ssl: false
 });
 
 // Manejar la conexión a la base de datos
