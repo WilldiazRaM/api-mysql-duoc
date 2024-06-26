dcb62984ce94e93c5b3d95831a242f7e9b83da8c
WilldiazRaM <wi.diaz@duocuc.cl>
2024-06-26 19:43:29 -0400

    AA

---
+++ models/CategoriasProductos.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+const pool = new Pool();
+
+class CategoriasProductos {
+    static async getAll() {
+        const res = await pool.query('SELECT * FROM "CategoriasProductos"');
+        return res.rows;
+    }
+
+    static async getById(id) {
+        const res = await pool.query('SELECT * FROM "CategoriasProductos" WHERE id = $1', [id]);
+        return res.rows[0];
+    }
+
+    static async create(nombre) {
+        const res = await pool.query('INSERT INTO "CategoriasProductos" (nombre) VALUES ($1) RETURNING *', [nombre]);
+        return res.rows[0];
+    }
+
+    static async update(id, nombre) {
+        const res = await pool.query('UPDATE "CategoriasProductos" SET nombre = $1 WHERE id = $2 RETURNING *', [nombre, id]);
+        return res.rows[0];
+    }
+
+    static async delete(id) {
+        await pool.query('DELETE FROM "CategoriasProductos" WHERE id = $1', [id]);
+    }
+}
+
+module.exports = CategoriasProductos;
