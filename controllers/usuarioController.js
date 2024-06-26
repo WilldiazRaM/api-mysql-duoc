0e4f3c9d61d64fbabaebcec48b691d478781645d
WilldiazRaM <wi.diaz@duocuc.cl>
2024-06-26 19:23:27 -0400

    aSDASFASFAS XD2

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,5 @@
 const BaseController = require('./baseController');
-const Usuarios = require('../models/usuarioModel');
+const Usuarios = require('../models/userModel');
 const { register } = require('./authController');
 
 class UsuarioController extends BaseController {
