5acac0498f97ac3c517c7e8457b1638db14d7e12
WilldiazRaM <wi.diaz@duocuc.cl>
2024-06-27 10:55:04 -0400

    test1

--- config/securityHeaders.js
+++ config/securityHeaders.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubdomains');
-    res.setHeader('Content-Security-Policy', "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://kit.fontawesome.com;");
+    res.setHeader('Content-Security-Policy', "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://kit.fontawesome.com; frame-src 'self' https://giphy.com;");
     res.setHeader('X-Frame-Options', 'SAMEORIGIN');
     res.setHeader('Permissions-Policy', "geolocation=(self), microphone=(), camera=()");
     next();
